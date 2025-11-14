cf60ce64d4680b701c30c1b2958c8aa5c1cd5e59
ElliotRichardson8 <up2204396@myport.ac.uk>
2025-11-14 17:55:25 +0000

    Added simple mutation for now
    let me know if its okay

---
+++ mutation.py
@@ -0,0 +1,15 @@
+import random
+
+def random_resetting_mutation(chromo, mutationRate, lower, upper):
+    """
+    Peforms randon resetting mutation on a floating-point chromosome
+    """
+    for x in range(len(chromo)):
+        if random.random() < mutationRate:
+            #replace gene with random number from boundaries
+            chromo[x] = random.uniform(lower,upper)
+    return chromo
+
+#test mutation
+for x in range(0,100):        
+    print(random_resetting_mutation([1.2,3.4,5.6], 0.1, -5, 5))

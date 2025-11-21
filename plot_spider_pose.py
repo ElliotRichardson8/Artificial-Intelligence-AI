0c8be49f0de0d8de0c3862844310ada6ee8526ba
Thomas <thomasisafisher@google.com>
2025-11-21 16:58:47 +0000

    Did some mining off camera.

--- plot_spider_pose.py
+++ plot_spider_pose.py
@@ -1,19 +1,21 @@
-import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
+
+import forward_kinematics
 
 # Declare constants -----------------------------------------------------------
 
+N_LEGS = 8
+
 # [coxa, femur, tibia]
 SEGMENT_LENGTHS = np.array([1.2, 0.7, 1.0])
+N_SEGMENTS = len(SEGMENT_LENGTHS)
 
 # Ellipse dimensions for body
 A = 1.5
 B = 1.0
 
-# Base angles
 BASE_ANGLES = np.deg2rad([45, 75, 105, 135, -135, -105, -75, -45])
-
-# Leg labels
 LEG_LABELS = ("L1", "L2", "L3", "L4", "R4", "R3", "R2", "R1")
 
 # Main function ---------------------------------------------------------------
@@ -33,4 +35,73 @@ def plot_spider_pose(angles):
     The first leg is L0 (front left), then legs go anticlockwise around the body
     until R0 (front right)
     """
-    # Important to remember that "left" is the spider's left, not the observer's left
+    # "left" is the spider's left, not the observer's left
+
+    # Validate input
+    if angles.size != N_LEGS * N_SEGMENTS:
+        raise ValueError(
+            f"Input angles must be a 1x{len(N_LEGS)*N_SEGMENTS} vector."
+            f"({N_SEGMENTS} angles per leg for {N_LEGS} legs.)")
+
+    # Create figure
+    fig = plt.figure()
+    # Add 3 dimensional axes
+    ax = fig.add_subplot(projection="3d")
+
+    for i in range(N_LEGS):
+
+        # "angles" is 24x1. We need to extract 3 angles at a time
+        idx = i*3
+        theta1 = angles[idx]
+        theta2 = angles[idx+1]
+        theta3 = angles[idx+2]
+
+        base_angle = BASE_ANGLES[i]
+        x_base = A * np.cos(base_angle)
+        y_base = B * np.sin(base_angle)
+        base_pos = np.array([x_base, y_base, 0])
+
+        joints = forward_kinematics.calculate_joint_positions(
+            base_pos,
+            base_angle,
+            (theta1, theta2, theta3),
+            SEGMENT_LENGTHS
+        )
+
+        # Plot legs -----------------------------------------------------------
+
+        # Separate out the different joint coordinates into 3 lists
+        # Corresponding to the x, y, z coordinates of the joints
+        jx, jy, jz = zip(*joints)
+
+        # Python list slicing is right-exclusive so we need the right index
+        # to be 1 greater than what we actually want to include
+        ax.plot(jx[0:2], jy[0:2], jz[0:2], "k-") # coxa; black line
+        ax.plot(jx[1:3], jy[1:3], jz[1:3], "b-") # femur; blue line
+        ax.plot(jx[2:], jy[2:], jz[2:], "r-") # tibia; red line
+        ax.plot(jx[3], jy[3], jz[3], "ro", markersize=5) # foot; red circle
+    
+        
+    # Create body. Idk how to fill it in
+    t = np.linspace(0, 2*np.pi, 100)
+    body_x = A * np.cos(t)
+    body_y = B * np.sin(t)
+    ax.plot(body_x, body_y, np.zeros(np.size(t)), "k-") # black line
+
+    # Add "head" marker to spider
+    ax.plot(A + 0.2, 0, 0, "r.") # red dot
+
+    # Add axis labels
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
+    # Set axis bounds
+    ax.set_xbound(-4, 4)
+    ax.set_ybound(-4, 4)
+    ax.set_zbound(-2, 2)
+    # Set camera position
+    ax.view_init(45, -45)
+    plt.show()
+
+
+plot_spider_pose(np.ones(24))

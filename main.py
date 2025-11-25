2c558eea40971bc91f126c41ac914906d44d9ce5
Oliver <106149435+SeaLand2@users.noreply.github.com>
2025-11-25 15:55:23 +0000

    remade main + animated the spider
    fitness function doesnt work that well
    
    animation tanks in performance after a while.

--- main.py
+++ main.py
@@ -1,54 +1,81 @@
 import crossover, fitness, initial, mutation, selection
 import forward_kinematics, plot_spider_pose
 import numpy as np
-import pygad
 
-# input 1x24 list of angles in radians
-def plot_spider_from_angles(angles):
-    plot_spider_pose.plot_spider_pose(np.array(angles))
+"fresh start"
+
+chromosomes = [] # 300 1x24 array 
+all_fitness_scores = [] # 300 fitness scores
+mutation_rate = 0.1
+pop_size = 50
+generations = 50
+
+
+def generate_population(parent1, parent2, pop_size):
+    offspring = []
+    while len(offspring) < pop_size - 2:
+        children = crossover.sp_crossover(parent1, parent2)
+        for child in children:
+            mutated_child = mutation.random_resetting_mutation(child, mutation_rate, -np.pi/2, np.pi/2)
+            offspring.append(mutated_child)
+            if len(offspring) >= pop_size - 2:
+                break
+    return offspring
 
 
 def main():
-    populations = []; # 300 1x24 lists at the end
-    all_fitness_scores = []; # lists at the end
-    parent_amount = 10 # number of parents to select from each generation
-    
-    plot_spider_from_angles([0.1, 0.2, -0.1, 0.3, -0.2, 0.1, -0.1, 0.4, -0.3, 0.2, 0.1, -0.2, -0.3, 0.2, 0.1, 0.4, -0.1, -0.2, 0.3, -0.4, 0.2, 0.1, -0.3, 0.2])
+    population = []
+    # start with 1 individual and mutate for gen 2 then do crossover+mutatuion for gen 3-300
+    individual = initial.initial_population(1, initial.joint_limits)
+    chromosomes.append(individual[0])
+    population.append(individual[0])
 
-    '''generate 300 populations for 300 generations
-    each population has 24 chromosomes
-    after each generation evaluate fitness of populations
-    select 10 parents using roulette wheel selection
-    perform crossover to produce 10 offspring
-    mutate offspring using random resetting mutation with mutation rate of 0.1
-    create new population with parents and mutated offspring
+    # mutate individual. then breed untill we have 300 population
+    mutated_individual = mutation.random_resetting_mutation(individual[0], 1, -np.pi/2, np.pi/2)
+    population.append(mutated_individual)
+    # crossover individual and mutated individual to create offspring untill we have 300 population
+    while len(population) < pop_size:
+        offspring = crossover.sp_crossover(population[0], population[1])
+        for child in offspring:
+            mutated_child = mutation.random_resetting_mutation(child, mutation_rate, -np.pi/2, np.pi/2)
+            population.append(mutated_child)
+    print(f"Initial Population Size: {len(population)}")
+
+    '''loop for 298 generations
+    evaluate fitness
+    select fittest individual and add to chromosomes
+    perform crossover to produce offspring
+    mutate offspring
+    add mutated offspring to chromosomes
     '''
-
-    # create initial population
-    population = initial.initial_population(20, initial.joint_limits)
-    for generation in range(200):
-        populations.append(population)
+    for generation in range(1, generations):
         gen_fitness_scores = []
         for pop in range(len(population)):
             fitness_score = fitness.evaluate_population_fitness(population[pop], population[pop-1])
             gen_fitness_scores.append(fitness_score)
-        print(f"Generation {generation} Population {len(population)} \nFitness: {gen_fitness_scores}")
-
         all_fitness_scores.append(gen_fitness_scores)
-        fitest_indivuals = selection.tournament_selection_population(population, gen_fitness_scores, 2)
-        plot_spider_from_angles(fitest_indivuals[0])
-        
-        selected_parents = selection.tournament_selection_population(population, gen_fitness_scores, parent_amount) # get 10 fitest parents
-        population = [] # reset population
-        for i in range(int(parent_amount/2)):
-            offspring = crossover.sp_crossover(selected_parents[i], selected_parents[i*2])
-        mutated_offspring = []
-        for child in offspring:
-            mutated_child = mutation.random_resetting_mutation(child, 0.1, -np.pi/2, np.pi/2)
-            population.append(mutated_child)
-        #population = selected_parents + mutated_offspring
+        fittest_individual = selection.tournament_selection_population(population, gen_fitness_scores, 1)[0]
+        chromosomes.append(fittest_individual)
 
+        # create new population
+        population = []
+
+        # create offspring from fittest individual and previous individual
+        while len(population) < pop_size:
+            offspring = crossover.sp_crossover(fittest_individual, chromosomes[generation-1])
+            for child in offspring:
+                mutated_child = mutation.random_resetting_mutation(child, mutation_rate, -np.pi/2, np.pi/2)
+                population.append(mutated_child)
+        if len(population) >= pop_size:
+            population = population[:pop_size] # ensure population size is maintained
+        print(f"End of Generation {generation} Population Size: {len(population)}")
+
+    # visualize fittest individual from each generation
+    #for angles in chromosomes:
+    #    print(f"Fittest Individual Angles: {angles}")
+    #    plot_spider_pose.plot_spider_pose(np.array(angles))
+    plot_spider_pose.plot_spider_animation(chromosomes)
 
 main()
+    
 
-plot_spider_from_angles([0.1, 0.2, -0.1, 0.3, -0.2, 0.1, -0.1, 0.4, -0.3, 0.2, 0.1, -0.2, -0.3, 0.2, 0.1, 0.4, -0.1, -0.2, 0.3, -0.4, 0.2, 0.1, -0.3, 0.2])    
--- plot_spider_pose.py
+++ plot_spider_pose.py
@@ -18,10 +18,6 @@ B = 1.0
 
 BASE_ANGLES = np.deg2rad([45, 75, 105, 135, -135, -105, -75, -45])
 LEG_LABELS = ("L1", "L2", "L3", "L4", "R4", "R3", "R2", "R1")
-frames = []
-leg_frames = []
-fig = plt.figure()
-
 
 # Main function ---------------------------------------------------------------
 def plot_spider_pose(angles):
@@ -37,16 +33,93 @@ def plot_spider_pose(angles):
     Angle 2 is the pitch of the femur
     Angle 3 is the pitch of the tibia
 
-    The first leg is L1 (front left), then legs go anticlockwise around the body
-    until R1 (front right)
+    The first leg is L0 (front left), then legs go anticlockwise around the body
+    until R0 (front right)
     """
     # "left" is the spider's left, not the observer's left
 
     # Validate input
     if angles.size != N_LEGS * N_SEGMENTS:
-        print("angles.size", angles.size, "\nangles:", angles)
         raise ValueError(
-            f"Input angles must be a 1x{N_LEGS*N_SEGMENTS} vector."
+            f"Input angles must be a 1x{len(N_LEGS)*N_SEGMENTS} vector."
+            f"({N_SEGMENTS} angles per leg for {N_LEGS} legs.)")
+
+    # Create figure
+    fig = plt.figure()
+    # Add 3 dimensional axes
+    ax = fig.add_subplot(projection="3d")
+
+    for i in range(N_LEGS):
+
+        # "angles" is 24x1. We need to extract 3 angles at a time
+        idx = i*3
+        theta1 = angles[idx]
+        theta2 = angles[idx+1]
+        theta3 = angles[idx+2]
+
+        base_angle = BASE_ANGLES[i]
+        x_base = A * np.cos(base_angle)
+        y_base = B * np.sin(base_angle)
+        base_pos = np.array([x_base, y_base, 0])
+
+        joints = forward_kinematics.calculate_joint_positions(
+            base_pos,
+            base_angle,
+            (theta1, theta2, theta3),
+            SEGMENT_LENGTHS
+        )
+
+        # Plot legs -----------------------------------------------------------
+
+        # Separate out the different joint coordinates into 3 lists
+        # Corresponding to the x, y, z coordinates of the joints
+        jx, jy, jz = zip(*joints)
+
+        # Python list slicing is right-exclusive so we need the right index
+        # to be 1 greater than what we actually want to include
+        ax.plot(jx[0:2], jy[0:2], jz[0:2], "k-") # coxa; black line
+        ax.plot(jx[1:3], jy[1:3], jz[1:3], "b-") # femur; blue line
+        ax.plot(jx[2:], jy[2:], jz[2:], "r-") # tibia; red line
+        ax.plot(jx[3], jy[3], jz[3], "ro", markersize=5) # foot; red circle
+
+        # Plot leg labels ---------
+
+        ax.text(jx[0], jy[0], jz[0], LEG_LABELS[i], fontweight="bold")
+    
+        
+    # Create body. Idk how to fill it in
+    t = np.linspace(0, 2*np.pi, 100)
+    body_x = A * np.cos(t)
+    body_y = B * np.sin(t)
+    ax.plot(body_x, body_y, np.zeros(np.size(t)), "k-") # black line
+
+    # Add "head" marker to spider
+    ax.plot(A + 0.2, 0, 0, "rs") # red dot
+
+    # Add axis labels
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
+    # Set axis bounds
+    ax.set_xbound(-4, 4)
+    ax.set_ybound(-4, 4)
+    ax.set_zbound(-2, 2)
+    # Set camera position
+    ax.view_init(45, -45)
+
+    ax.set_aspect("equal")
+    plt.show()
+
+fig = plt.figure()
+def generate_spider_frame(angles):
+    '''Generate a 3D plot of a spider pose and return the figure object.
+
+    Input:
+        angles: 1x24 vector of joint angles in radians'''
+
+    if angles.size != N_LEGS * N_SEGMENTS:
+        raise ValueError(
+            f"Input angles must be a 1x{len(N_LEGS)*N_SEGMENTS} vector."
             f"({N_SEGMENTS} angles per leg for {N_LEGS} legs.)")
 
     # Add 3 dimensional axes
@@ -88,8 +161,6 @@ def plot_spider_pose(angles):
         # Plot leg labels ---------
 
         ax.text(jx[0], jy[0], jz[0], LEG_LABELS[i], fontweight="bold")
-
-        leg_frames.append(ax)
     
         
     # Create body. Idk how to fill it in
@@ -113,20 +184,33 @@ def plot_spider_pose(angles):
     ax.view_init(45, -45)
 
     ax.set_aspect("equal")
-    frames.append(ax)
+    return ax
 
-    plt.show()
+    
+ax = fig.add_subplot(projection="3d")
+def animate(i):
+    #animate spiders 
+    chromosome = gloChromosome
+    ax.clear()
+    plt.cla()
+    frame = generate_spider_frame(chromosome[i])
+    #clear title and add new title
+    plt.title(f"Generation {i+1}")
+    patches.append([frame])
+    return patches
 
-def update(frames):
-        ax.cla()  # Clear the axes
-        for frame in frames:
-            ax = frame.gca(projection='3d')
-            for line in ax.get_lines():
-                ax.add_line(line)   
+def plot_spider_animation(chromosome):
+    '''Plot a 3D animation of a spider moving through a sequence of poses.
 
-def show_spider_animation():
-    """Displays an animation of the spider moving through the recorded frames."""
-    from matplotlib.animation import FuncAnimation
+    Input:
+        angle_sequence: 300 list of 1x24 vectors of joint angles in radians'''
 
-    ani = animation.FuncAnimation(fig, update, frames=frames, interval=200)
+    frame_amount = len(chromosome)
+    spiders = [plt.plot([], [])[0] for _ in range(frame_amount)]
+    global patches 
+    patches = spiders
+    global gloChromosome
+    gloChromosome = chromosome
+
+    anim = animation.FuncAnimation(fig, animate, frames=len(chromosome), interval=100)
     plt.show()

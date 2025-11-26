f0b9562e5dabe7f364eb094837be1c561009e714
Thomas <thomasisafisher@google.com>
2025-11-26 05:57:19 +0000

    Changed joint_limits and some other touches

--- initial.py
+++ initial.py
@@ -3,33 +3,33 @@ import numpy as np
 np.set_printoptions(threshold=sys.maxsize, suppress=True, precision=4)
 
 """
-Joint limits are set in place to prevent impossible joint angles
-"""
-joint_limits = [
-    (-0.78, 0.78), # coxa
-    (-1.047, 1.047), # femur
-    (-1.57, 1.57) # tibia
-]
+Joint limits are set in place to keep joint angles within plausible range
 
-def generate_chromosome(joint_limits=joint_limits):
+These values are sorta pulled from nowhere but they work pretty well. 
+"""
+JOINT_LIMITS = np.array([
+    (-1, 1), # coxa
+    (-2, 0.5), # femur
+    (-2, 0.5) # tibia
+])
+
+def generate_chromosome(joint_limits=JOINT_LIMITS):
     chromosome = np.zeros(shape=(8, 3))
     for leg in range(8):
         for segment in range(3):
-            low, high = joint_limits[segment]
-            angle = np.random.uniform(low, high)
+            angle = np.random.uniform(*joint_limits[segment])
             chromosome[leg, segment] = angle
 
     return chromosome
 
 # TODO have these constants not be hardcoded
-def initial_population(pop_size, joint_limits=joint_limits):
+def initial_population(pop_size, joint_limits=JOINT_LIMITS):
     population = np.zeros(shape=(pop_size, 8, 3))
     for i, individual in enumerate(population):
         population[i] = generate_chromosome(joint_limits)
 
     return population
 
-
 def test():
-    population = initial_population(50, joint_limits)
+    population = initial_population(50)
     print(population[1])
